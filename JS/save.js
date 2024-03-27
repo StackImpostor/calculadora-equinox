c66eca84c0b3f1a7ac57ffa4a5cee55a7952b4c1
David <davidcantero223@gmail.com>
2024-03-27 11:25:29 +0100

    Update save.js
    Arreglada carga de datos cuando no hay datos guardados

--- JS/save.js
+++ JS/save.js
@@ -15,6 +15,11 @@ let e = Number.parseInt(localStorage.getItem("energia-actual"));
 let n = localStorage.getItem("notas-equinox");
 let el = localStorage.getItem("elemento");
 
+if(isNaN(v)) v = personaje.estadisticas.vida;
+if(isNaN(m)) m = personaje.estadisticas.mana;
+if(isNaN(e)) e = personaje.estadisticas.energia;
+if(el == undefined) el = "hielo";
+
 vida = v;
 mana = m;
 energia = e;
