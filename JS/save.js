7658984a92b92926b3e6ec124a67a7462392ebde
David <davidcantero223@gmail.com>
2024-03-27 02:55:19 +0100

    fin de la 2.0?

--- JS/save.js
+++ JS/save.js
@@ -5,6 +5,7 @@ function saveData(){
   localStorage.setItem("mana-actual",mana);
   localStorage.setItem("energia-actual",energia);
   localStorage.setItem("notas-equinox",notas);
+  localStorage.setItem("elemento",elementoPorDefecto);
 }
 
 function loadData(){
@@ -12,9 +13,11 @@ let v = Number.parseInt(localStorage.getItem("vida-actual"));
 let m = Number.parseInt(localStorage.getItem("mana-actual"));
 let e = Number.parseInt(localStorage.getItem("energia-actual"));
 let n = localStorage.getItem("notas-equinox");
+let el = localStorage.getItem("elemento");
 
 vida = v;
 mana = m;
 energia = e;
 document.getElementById("notas").value = n;
+cambiarElementoPorDefecto(el);
 }
